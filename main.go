fa9568c3943f722fc5803528000d77d344303140
Ibrahim Abiola Ibrahim <abiola89@gmail.com>
2014-12-15 09:25:55 +0100

    changed error code to 500

--- main.go
+++ main.go
@@ -44,6 +44,6 @@ func markHandler(rw http.ResponseWriter, req *http.Request) {
 }
 
 func serveErr(rw http.ResponseWriter, req *http.Request, err error) {
-	rw.WriteHeader(505)
+	rw.WriteHeader(500)
 	rw.Write([]byte(fmt.Sprint("Internal Server Error", err)))
 }
